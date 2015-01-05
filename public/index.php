5d05d03321815ef84f2f5ffb0030d5bdf158f267
Antonio Requena <razondota@gmail.com>
2015-01-04 20:05:27 -0430

    Added monolog for logging

--- public/index.php
+++ public/index.php
@@ -13,8 +13,16 @@ if (file_exists(__DIR__.'/../.env'))
   Dotenv::load(__DIR__.'/../');
 }
 
+$logger = new \Flynsarmy\SlimMonolog\Log\MonologWriter(array(
+  'handlers' => array(
+    new \Monolog\Handler\StreamHandler('../storage/logs/'.date('Y-m-d').'.log'),
+  ),
+));
+
 //New instance of Slim
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(array(
+  'log.writer' => $logger
+));
 
 //Routes file
 include '../src/routes.php';
