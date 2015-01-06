7ac24138d2328a5dbb3a6290acf21d25f08d2960
Antonio Requena <razondota@gmail.com>
2015-01-06 05:32:11 -0430

    Added blade templating language

--- public/index.php
+++ public/index.php
@@ -15,6 +15,7 @@ if (file_exists(__DIR__.'/../.env'))
 
 //New Instance of Slim
 $app = new \Slim\Slim(array(
+  'view' => new \Slim\Views\Blade(),
   'templates.path' => '../resources/templates',
 ));
 
@@ -24,6 +25,13 @@ $app->container->singleton('log', function () {
   $log->pushHandler(new \Monolog\Handler\StreamHandler('../storage/logs/app.log', \Monolog\Logger::DEBUG));
   return $log;
 });
+
+// Using blade for templating
+$view = $app->view();
+$view->parserOptions = array(
+  'debug' => true,
+  'cache' => '../storage/cache'
+);
 
 //Routes file
 include '../src/routes.php';
